5efd7748070e97a673e37ea076344bd57f72df19
Gui Silva <adrianon10@gmail.com>
2023-11-10 04:16:31 -0300

    feat: add shadcn-ui label component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,83 @@
+import { FileVideo, Github, Upload } from 'lucide-react'
+
 import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { Textarea } from '@/components/ui/textarea'
+import { Label } from '@/components/ui/label'
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
-      <Button>Clique</Button>
+    <main className="flex min-h-screen flex-col">
+      <header className="flex items-center justify-between border-b px-6 py-3">
+        <h1 className="text-xl font-bold">upload A.I</h1>
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💚
+          </span>
+          <Separator orientation="vertical" className="h-6" />
+          <Button variant="outline">
+            <Github className="mr-2 h-4 w-4" />
+            Github
+          </Button>
+        </div>
+      </header>
+
+      <section className="flex flex-1 gap-6 p-6">
+        <div className="flex flex-1 flex-col gap-4">
+          <div className="grid flex-1 grid-rows-2 gap-4">
+            <Textarea
+              placeholder="Inclua o prompt para IA..."
+              className="resize-none p-4 leading-relaxed"
+            />
+            <Textarea
+              placeholder="Resultado gerado pela IA"
+              readOnly
+              className="resize-none p-4 leading-relaxed"
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável{' '}
+            <code className="text-violet-400">{'{transcription}'}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do vídeo selecionado
+          </p>
+        </div>
+
+        <aside className="w-80 space-y-6 overflow-y-auto">
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="flex aspect-video w-full cursor-pointer flex-col items-center justify-center gap-2 rounded-md border border-dashed text-sm text-muted-foreground hover:bg-primary/5"
+            >
+              <FileVideo className="h-4 w-4" />
+              Selecione um vídeo
+            </label>
+            <input
+              type="file"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
+            <Separator />
+            <div className="space-y-2">
+              <Label htmlFor="transcription_prompt">
+                Prompt de transcrição
+              </Label>
+              <Textarea
+                id="transcription_prompt"
+                className="h-20 resize-none leading-relaxed"
+                placeholder="Inclua palavras chave mencionadas no vídeo separada por vírgula (,)"
+              />
+            </div>
+            <Button type="submit" className="w-full">
+              Carregar vídeo
+              <Upload className="ml-2 h-4 w-4" />
+            </Button>
+          </form>
+          <Separator />
+          form.
+        </aside>
+      </section>
     </main>
   )
 }
